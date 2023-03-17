3d127509b7c2146f621defa7aa6b46efca83862f
balaji24k <balaji325s@gmail.com>
2023-03-17 14:58:29 +0530

    Storing and Deleting Task-11

--- src/App.js
+++ src/App.js
@@ -13,25 +13,28 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-api-calls-f5b35-default-rtdb.firebaseio.com/movies.json");
       if(!response.ok){
         throw new Error('Something went wrong....Retrying')
         } 
       const data = await response.json();
-  
-      const transformedMovies  = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+          loadedMovies.push({
+            id : key,
+            title : data[key].title,
+            openingText : data[key].openingText,
+            releaseDate : data[key].releaseDate
+          });
+      };
+      setMovies(loadedMovies);
     }
+
     catch(error) {
       setError(error.message)
-      setTimeout(fetchMoviesHandler,5000)
+      // setTimeout(fetchMoviesHandler,5000)
     }
     setIsLoading(false);
   },[]);
@@ -41,8 +44,18 @@ function App() {
   }, [fetchMoviesHandler]);
 
 
-  const addMovieHandler = (movie) => {
-    console.log(movie);
+  const addMovieHandler = async (movie) => {
+    const response= await fetch("https://react-api-calls-f5b35-default-rtdb.firebaseio.com/movies.json", {
+      method : "POST",
+      body : JSON.stringify(movie),
+      headers : {
+        'Content-Type' : 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchMoviesHandler();
+    
   }
 
   const cancelHandler = () => {
@@ -53,7 +66,7 @@ function App() {
   let content =<p>Found no movies</p>
 
   if(movies.length > 0){
-    content = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} setMovies={setMovies} />
   }
   if(error){
     content=<p>{error}</p>
